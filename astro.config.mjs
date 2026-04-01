ffde5f9513600afb9cebc662bdaee38ff17838ed
iyibilgilernet <olmadix@gmail.com>
2026-04-01 13:14:57 +0300

    Update astro.config.mjs configuration
    Removed MDX and sitemap integrations, added base path.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-	site: 'https://iyibilgilernet.github.io', // Buraya Kendi GitHub kullanıcı adını veya site adresini yaz
-	integrations: [mdx(), sitemap()],
+	site: 'https://iyibilgilernet.github.io',
+	base: '/', // Eğer siten ana dizindeyse burası böyle kalsın
 });
